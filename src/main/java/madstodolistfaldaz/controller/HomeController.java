4457148131d34483511d30cbe9baf0070833bd0e
Fernando Aldaz <118292813+Josxx24@users.noreply.github.com>
2025-06-04 17:48:48 -0500

    Añadida vista y controller 'about'

---
+++ src/main/java/madstodolistfaldaz/controller/HomeController.java
@@ -0,0 +1,15 @@
+package madstodolistfaldaz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
+    }
+
+}
